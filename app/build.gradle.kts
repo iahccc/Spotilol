fdd386934a97418d9acd119e2c1fc33f113755a5
iahc <qingchaihuang@163.com>
2026-08-29 01:16:00 +0800

    Bump version to 1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.project.lol"
         minSdk = 28
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0.11"
+        versionCode = 11
+        versionName = "1.0.12"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a")
         }
